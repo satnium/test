15320878b4dd3d83c035bd84d0456fdecf2ea68d
satnium <satyamagnihotri789@gmail.com>
2022-03-17 03:05:08 +0530

    login_prompt demo

---
+++ app/src/main/java/com/example/loginportal/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.loginportal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getSupportActionBar().hide();
+
+        setContentView(R.layout.activity_main);
+    }
+}
